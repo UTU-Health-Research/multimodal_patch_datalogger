978d31967dea2f40cf9d29cb71c724ba28c8240d
unknown <andiva@utu.fi>
2025-10-28 12:33:08 +0200

    I2C read/write functions into IMU header to be used by temp sensor Temperature sensor init and data read logic - first implementation

--- src/imu.h
+++ src/imu.h
@@ -14,3 +14,7 @@ void imu_start(void);
 
 // Get the latest IMU data - thread-safe access function
 void imu_get_latest_data(imu_sample_t *imu1_data, imu_sample_t *imu2_data);
+
+// I2C functions
+esp_err_t i2c_read_register_with_timeout(i2c_port_t i2c_port, uint8_t addr, uint8_t reg, uint8_t *data, size_t len);
+esp_err_t i2c_write_register_with_timeout(i2c_port_t i2c_port, uint8_t addr, uint8_t reg, const uint8_t *data, size_t len); 
