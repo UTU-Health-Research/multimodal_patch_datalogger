c1e3f020eddddc66a313e340f19a20dc8d6926ad
unknown <andiva@utu.fi>
2026-05-08 15:23:21 +0300

    Public declaration of temperature sensor detection function

--- src/i2c_sensors.h
+++ src/i2c_sensors.h
@@ -9,11 +9,21 @@
 #include "config.h"
 #include <string.h>
 
+// Detected temperature sensor type
+typedef enum {
+    TEMP_SENSOR_NONE = 0,
+    TEMP_SENSOR_TMP117,
+    TEMP_SENSOR_MAX30205
+} temp_sensor_type_t;
+
 // Initialize and start the IMU task
 void sensors_start(void);
 
 // Get the latest IMU data - thread-safe access function
 void sensors_get_latest_data(imu_sample_t *imu1_data, imu_sample_t *imu2_data, float *temperature);
+
+// Exposed so other modules can check what was detected
+temp_sensor_type_t sensors_get_temp_sensor_type(void);
 
 // I2C functions
 esp_err_t i2c_read_register_with_timeout(i2c_port_t i2c_port, uint8_t addr, uint8_t reg, uint8_t *data, size_t len);
